3d152d67687b8461cb5ec1e3ce80bff563952748
bhavani-Zemoso <bhavani.somanchi@zemosolabs.com>
2023-03-08 15:38:41 +0530

    Dynamic routes and advanced parameters

--- express-js/controllers/shop.js
+++ express-js/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
 	// console.log(adminData.products);
@@ -7,6 +8,17 @@ exports.getProducts = (req, res, next) => {
 		res.render('shop/product-list', {
 			prods: products,
 			docTitle: 'All Products',
+			path: '/products',
+		});
+	});
+};
+
+exports.getProduct = (req, res, next) => {
+	const productId = req.params.productId;
+	Product.findById(productId, (product) => {
+		res.render('shop/product-detail', {
+			docTitle: product.title,
+			product: product,
 			path: '/products',
 		});
 	});
@@ -23,11 +35,39 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-	res.render('shop/cart', {
-		docTitle: 'Your Cart',
-		path: '/cart',
-	});
+	Cart.getCart(cart => {
+		Product.fetchAll(products => {
+			const cartProducts = [];
+			for(const product of products) {
+				const cartProductData = cart.products.find(prod => prod.id === product.id)
+				if (cartProductData) {
+					cartProducts.push({productData: product, quantity: cartProductData.quantity});
+				}
+			}
+			res.render('shop/cart', {
+				docTitle: 'Your Cart',
+				path: '/cart',
+				products: cartProducts
+			});
+		})
+	})
 };
+
+exports.postCart = (req, res, next) => {
+	const productId = req.body.productId;
+	Product.findById(productId, (product) => {
+		Cart.addProduct(productId, product.price);
+	})
+	res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+	const productId = req.body.productId;
+	Product.findById(productId, product => {
+		Cart.deleteProduct(productId, product.price);
+		res.redirect('/cart');
+	})	
+}
 
 exports.getCheckout = (req, res, next) => {
 	res.render('shop/checkout', {
@@ -40,5 +80,5 @@ exports.getOrders = (req, res, next) => {
 	res.render('shop/orders', {
 		path: '/orders',
 		docTitle: 'Your Orders',
-	})
-}
+	});
+};
