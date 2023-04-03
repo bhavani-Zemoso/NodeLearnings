acb0bb9788004ca3148b96dd77567adc5037fef7
bhavani-Zemoso <bhavani.somanchi@zemosolabs.com>
2023-04-03 09:27:52 +0530

    Testing

--- seeder/backend/src/types/express/index.d.ts
+++ seeder/backend/src/types/express/index.d.ts
@@ -4,13 +4,13 @@ import * as jwt from 'jsonwebtoken'
 
 declare global {
 	namespace Express {
-		interface Request {
+		export interface Request {
 			user: Record<string, any>;
 			userId: string;
 			token: string | jwt.JwtPayload;
 		}
 	}
-	interface Error {
+	export interface Error {
 		statusCode?: number;
 		data?: ValidationError[];
 	}
